9e72c791c7a9bf14b075ff44b520175e219d18c9
Adwitiya Paul <adwitiyapaul8@gmail.com>
2020-12-26 10:16:51 +0530

    Solution.cpp

---
+++ Strings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a,b;
+    cin>>a>>b;
+    cout<<a.size()<<" "<<b.size()<<endl;
+    cout<<a+b<<endl;
+    char c;
+    c=a[0];
+    a[0]=b[0];
+    b[0]=c;
+    cout<<a<<" "<<b;
+    return 0;
+}
