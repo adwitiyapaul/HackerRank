ac0dbc76b6003369450d3b26121b470c71c3e69b
Adwitiya Paul <adwitiyapaul8@gmail.com>
2020-12-26 09:56:59 +0530

    Basic Data Types.cpp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int p;
+    long q;
+    char r;
+    float s;
+    double t;
+    cin>>p>>q>>r>>s>>t;
+    cout<<p<<endl;
+    cout<<q<<endl;
+    cout<<r<<endl;
+    printf("%.3f \n", s);
+    printf("%.9lf", t);
+    return 0;
+}
