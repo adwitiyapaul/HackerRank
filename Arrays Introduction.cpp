2be4f5de5d7c716ee119efcedd9a4b883b38ea42
Adwitiya Paul <adwitiyapaul8@gmail.com>
2020-12-26 10:14:35 +0530

    Solution.cpp

---
+++ Arrays Introduction.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int i, n, a[10000];
+    cin>>n;
+    for (i=0;i<n;i++)
+    {
+        cin>>a[i];
+    } 
+    for (i=n-1;i>=0;i--)
+    {
+        cout<<a[i]<<" ";
+    }  
+    return 0;
+}
