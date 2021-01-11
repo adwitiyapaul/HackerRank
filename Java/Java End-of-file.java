5446fc3e0580747bcb7a1219ff292c618f678bf1
Adwitiya Paul <adwitiyapaul8@gmail.com>
2021-01-11 11:51:13 +0530

    Sol.java

---
+++ Java/Java End-of-file.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int i=0;
+        while(sc.hasNext()==true)
+        {
+            i++;
+            System.out.println(i+" "+sc.nextLine());
+        }
+    }
+}
